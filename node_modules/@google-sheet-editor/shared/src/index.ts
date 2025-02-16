a9b4fe91a09e0af990957a9e2d97a34c10f89f76
Ido <ido.sarue@travelfactory.fr>
2025-02-16 13:41:02 +0200

    fix errors

---
+++ node_modules/@google-sheet-editor/shared/src/index.ts
@@ -0,0 +1,2 @@
+export { Job, ALLOWED_STATUS, ALLOWED_JOB_TITLES } from "./models/Job";
+export { jobSchema } from "./validations/jobValidation";
---
+++ shared/dist/validations/jobValidation.d.ts
@@ -0,0 +1,1 @@
+export declare const jobSchema: any;
